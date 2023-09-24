2a778192b4694521f03acaef1b1820dba16fd413
abd.alsadi1994 <abd.alsadi1994@gmail.com>
2023-09-24 08:57:23 -0700

    first commit

---
+++ KmnlkExcelAddins/Helpers/MainHelper.cs
@@ -0,0 +1,35 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmnlkExcelAddins.Helpers
+{
+    public class MainHelper
+    {
+        public static void SaveAsPdf()
+        {
+            string desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            Excel.Workbook workbook = Globals.ThisAddIn.Application.ActiveWorkbook;
+           string sfileName_Document = workbook.Name;
+            string sPath = workbook.Path;
+            if (sPath == "" || sPath == null)
+            {
+                sPath = desktopFolder;
+            }
+            string sFullpath_pdf = sPath + "\\" + sfileName_Document + ".pdf";
+
+            workbook.ExportAsFixedFormat(
+                   Microsoft.Office.Interop.Excel.XlFixedFormatType.xlTypePDF,
+                   sFullpath_pdf,
+                   Microsoft.Office.Interop.Excel.XlFixedFormatQuality.xlQualityStandard,
+                   true,
+                   true,
+                   1,
+                   10,
+                   false);
+        }
+    }
+}
---
+++ KmnlkExcelAddins/Ribbons/ConvertExcelToPdf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+//----< Excel Addin >----
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using KmnlkExcelAddins.Helpers;
+//----</ Excel Addin >----
+namespace KmnlkExcelAddins.Ribbons
+{
+    public partial class ConvertExcelToPdf
+    {
+        private void ConvertExcelToPdf_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btn_SaveAsPdf_Click(object sender, RibbonControlEventArgs e)
+        {
+            MainHelper.SaveAsPdf();
+        }
+    }
+}
